fc07f12adca7c62e48cd16afbf03cfa72a504c84
Krish Kochar <krishkochar@gmail.com>
2023-10-14 23:31:38 -0400

    More Updates

--- htv.py
+++ htv.py
@@ -5,6 +5,9 @@ import os
 import logging
 from dotenv import load_dotenv
 import cohere
+import torch
+import random
+from random import gauss
 
 # Logging
 logging.getLogger("complete").setLevel(logging.WARNING)
@@ -125,6 +128,163 @@ def cosine():
 def max_cosine():
     cosine_array = cosine()
     return cosine_array.max()
+
+
+
+# Set a random seed for reproducibility
+random.seed(0)
+torch.manual_seed(0)
+
+# Create empty lists to store the data
+people_data = []
+
+# Generate data for 100 people
+for _ in range(100):
+    income = random.randint(30000, 200000)  # Income between $30,000 and $120,000\
+    income_num=0
+    if income <40000:
+      income_num =1
+    elif income< 53359:
+      income_num=2
+    elif income <106717:
+      income_num=3
+    elif income <165430:
+      income_num = 4
+    elif income <235675:
+      income_num =5
+    else:
+      income_num =6
+    credit_score = random.randint(300, 900)  # Credit score between 400 and 850
+    asset_value = random.uniform(10000, 500000)  # Asset value between $10,000 and $500,000
+    debt_value = random.uniform(0, asset_value)  # Debt value is less than or equal to asset value
+    risk_tolerance = random.choice([1,1.5,2,2.5,3])  # Risk tolerance level (1, 2, 3)
+
+ 
+    # Create a dictionary representing a person's data
+    person = {
+        "Income Score": income_num,
+        "Credit Score": credit_score,
+        "Asset Value": asset_value,
+        "Debt Value": debt_value,
+        "Risk Tolerance": risk_tolerance
+    }
+
+    people_data.append(person)
+
+ 
+# Apply transformations to income, asset value, and debt value
+
+# making pandas file
+people_df = pd.DataFrame(people_data)
+people_df['ID'] = range(1, len(people_df) + 1)
+people_df.head(100)
+pd.set_option('display.max_rows', None)
+
+# Display the first few records
+
+# for i in range(100):
+
+#     print(f"Person {i + 1}: {people_data[i]}")
+
+ 
+
+# You can access the data for each person using people_data[index]
+
+# For example, to access the risk tolerance score of the first person:
+
+# risk_tolerance_of_first_person = people_data[0]["Risk Tolerance"]
+
+# people_df
+
+fund_data = pd.read_csv(r'/content/sample_data/HV_8_Fund_Data.csv')
+
+fund_data['hash values'] = range(1, len(fund_data) + 1)
+
+# fund_data
+
+
+# Convert the DataFrame to a string
+
+fund_data_string = fund_data.to_string(index=False, header=False)
+
+ 
+
+# Display the resulting string
+
+# print(fund_data_string)
+
+people_df_string = people_df.to_string(index=False, header=False)
+
+# people_df_string
+
+# !pip install cohere
+
+ 
+
+import cohere
+
+co = cohere.Client(API_KEY)
+
+text = people_df_string
+
+lines = text.splitlines()
+
+var_data= range(1, 100)
+
+ 
+
+import random 
+
+data_mapped = []
+
+
+for i in range(100):
+    x = random.randint(1, 50)
+    y = random.randint(1, 50)
+    z = random.randint(1, 50)
+
+    # Create a dictionary for the new row
+    new_row = {
+        'Person_ID': i,
+        'Priority 1': x,
+        'Priority 2': y,
+        'Priority 3': z
+    }
+
+    # Append the new row to the list
+
+    data_mapped.append(new_row)
+
+ 
+
+# Create a DataFrame from the list of dictionaries
+
+data_mapped_df = pd.DataFrame(data_mapped)
+
+ 
+
+# Print the DataFrame to see the result
+
+# print(data_mapped_df)
+
+ 
+
+# # Display each line
+
+# # for line in lines:
+
+#     # print(line)
+
+# response = co.generate(
+
+#   prompt = " Suppose you are a pesron having Income-Score          Credit-Score Asset-Value Debt-Value Risk-Tolerance ID as " + line + " respectively, provide me three row numbers of the assets  in terms of priority from " + fund_data_string + " in which you are likely to invest in"
+
+#       # prompt= "using the"+people_df_string+"build me another dataset where each value in"+people_df_string +"rates each value of"+fund_data_string+"based on how likely each person in"+people_df_string+"to buy the given investment option in"+fund_data_string+"the ratings should be from 1-5 where 1 is least likely to buy and 5 is most likely to buy"
+
+# )
+
+# print(response[0])
+
 
 
 
